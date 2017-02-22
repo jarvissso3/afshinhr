cc0b35547be3aeeb7a1de734480f735cd1055809
jarvissso3 <afshinn730@gmail.com>
2017-02-22 04:52:09 -0800

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -75,34 +75,13 @@ function create_config( )
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
-    info_text = [[》Beyond Reborn v3.0
+    info_text = [[》 Reborn v3.0
 An advanced administration bot based on https://valtman.name/telegram-cli
 
-》https://github.com/BeyondTeam/BDReborn 
+》https://github.com/jarvissso3/afshinhr 
 
 》Admins :
-》@SoLiD ➣ Founder & Developer《
-》@Makan ➣ Developer《
-》@Rixel ➣ Developer 《
-》@Exacute ➣ Developer《
-》@To0fan ➣ Developer《
-》@Tele_Sudo ➣ Developer《
-》@CiveY ➣ Developer
-》@ArmanDev ➣ Manager《
-》@MrPars ➣ Manager《
-
-》Special thanks to :
-》@Vysheng
-》@MrHalix
-》@K_a_I_i_I_i_n_u_x
-》@Nero_Dev
-》And Beyond Team Members
-
-》Our channel :
-》@BeyondTeam《
-
-》Our website :
-》http://BeyondTeam.ir
+》@joker_hr ➣ Founder & Developer
 ]],
   }
   serialize_to_file(config, './data/config.lua')
